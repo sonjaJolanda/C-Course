b1a297b3a110f7da79a6f55a39f11b5cd9e02eac
Sonja Klein <so161kle@htwg-konstanz.de>
2022-10-05 19:16:30 +0200

    hw2 done

--- lecture5/week5_3_HW2.cpp
+++ lecture5/week5_3_HW2.cpp
@@ -7,37 +7,35 @@ void swap(int &a, int &b) {
     b = temp;
 }
 
- void printALlPermutations(int permutation[], int helper, int n) {
+void permute(int a[], int helper, int n) {
 
-    if (helper == 1) { //print
-        for (int i = 0; i < n; i++)
-            std::cout << permutation[i];
-        printf(" ");
-        return;
-    }
-
-    for (int i = 0; i < helper; i++) {
-        printALlPermutations(permutation, helper - 1, n);
-        // if helper is odd, swap 0th i.e (first) and (helper-1)th i.e (last) element
-        if (helper % 2 == 1)
-            swap(permutation[0], permutation[helper - 1]);
-        else
-            swap(permutation[i], permutation[helper - 1]); // If helper is even, swap ith and (helper-1)th i.e (last) element
+    if (helper == n) {
+        for (int i = 0; i <= n; i++) {
+            std::cout << a[i];
+        }
+        std::cout << " ";
+    } else {
+        // Permutations made
+        for (int i = helper; i <= n; i++) {
+            swap(a[helper], a[i]);
+            permute(a, helper + 1, n);
+            swap(a[helper], a[i]);
+        }
     }
 }
 
-void printAllPermutationsFromOneToN(int n) {
+void permutateNumbers(int n) {
     int permutation[n];
     for (int i = 1; i <= n; i++) {
         permutation[i - 1] = i;
     }
-    printALlPermutations(permutation, n, n);
+    permute(permutation, 0, n - 1);
 }
 
 int main() {
     int nEntered;
     std::cout << ">> Please enter a number: ";
     std::cin >> nEntered;
-    printAllPermutationsFromOneToN(nEntered);
+    permutateNumbers(nEntered);
     return 0;
 }
--- lecture5/week5_4_HW2.cpp
+++ lecture5/week5_4_HW2.cpp
@@ -14,7 +14,11 @@ int sum_down_rec(int x) {
 int sum_down_it(int x) {
     int temp = 1;
     int y;
-    for(int i = 0; i < x; i++){
+
+    if (x <= 0)
+        return temp;
+
+    for (int i = 0; i < x; i++) {
         y = i + temp;
         y = y + temp;
         temp = y;
@@ -23,7 +27,23 @@ int sum_down_it(int x) {
 }
 
 int main() {
-    std::cout << "rec " << sum_down_rec(5) << std::endl;
-    std::cout << "it " << sum_down_it(5);
+    std::cout << "-1: recursive: " << sum_down_rec(-1) << " | iterative " << sum_down_it(-1) << "\n";
+    std::cout << "0: recursive: " << sum_down_rec(0) << " | iterative " << sum_down_it(0) << "\n";
+    std::cout << "1:  recursive: " << sum_down_rec(1) << " | iterative " << sum_down_it(1) << "\n";
+    std::cout << "2:  recursive: " << sum_down_rec(2) << " | iterative " << sum_down_it(2) << "\n";
+    std::cout << "3:  recursive: " << sum_down_rec(3) << " | iterative " << sum_down_it(3) << "\n";
+    std::cout << "4:  recursive: " << sum_down_rec(4) << " | iterative " << sum_down_it(4) << "\n";
+    std::cout << "5:  recursive: " << sum_down_rec(5) << " | iterative " << sum_down_it(5) << "\n";
+    std::cout << "6:  recursive: " << sum_down_rec(6) << " | iterative " << sum_down_it(6) << "\n";
+    std::cout << "7:  recursive: " << sum_down_rec(7) << " | iterative " << sum_down_it(7) << "\n";
+    std::cout << "8:  recursive: " << sum_down_rec(8) << " | iterative " << sum_down_it(8) << "\n";
+    std::cout << "9:  recursive: " << sum_down_rec(9) << " | iterative " << sum_down_it(9) << "\n";
+    std::cout << "10: recursive: " << sum_down_rec(10) << " | iterative " << sum_down_it(10) << "\n";
+    std::cout << "11: recursive: " << sum_down_rec(11) << " | iterative " << sum_down_it(11) << "\n";
+    std::cout << "12: recursive: " << sum_down_rec(12) << " | iterative " << sum_down_it(12) << "\n";
+    std::cout << "13: recursive: " << sum_down_rec(13) << " | iterative " << sum_down_it(13) << "\n";
+
+    std::cout << "\n18: recursive: " << sum_down_rec(18) << " | iterative " << sum_down_it(18) << "\n";
+    std::cout << "19: recursive: " << sum_down_rec(19) << " | iterative " << sum_down_it(19) << "\n";
     return 0;
 }
