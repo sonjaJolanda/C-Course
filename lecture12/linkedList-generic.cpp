b1170beb9960a618ba34f9f06868ece962047269
Sonja <sonjak982@gmail.com>
2022-11-28 15:20:01 +0100

    added homework 6

--- lecture12/linkedList-generic.cpp
+++ lecture12/linkedList-generic.cpp
@@ -2,41 +2,41 @@
 
 using namespace std;
 
-template <class TYPE> 
+template<class TYPE>
 class ListOf {
 public :
-	TYPE data;
-	ListOf<TYPE>* next;
+    TYPE data;
+    ListOf<TYPE> *next;
 
-	// constructor
-	ListOf<TYPE>(TYPE d) {
-		data = d;
-		next = NULL;
-	}
+    // constructor
+    ListOf<TYPE>(TYPE d) {
+        data = d;
+        next = NULL;
+    }
 
-	ListOf<TYPE>* append(TYPE i) {
-		ListOf<TYPE>* newItem = new ListOf<TYPE>(i);
-		next = newItem;
-		return newItem;
-	}
+    ListOf<TYPE> *append(TYPE i) {
+        ListOf<TYPE> *newItem = new ListOf<TYPE>(i);
+        next = newItem;
+        return newItem;
+    }
 
-	void printList(){
-		ListOf<TYPE>* ptr = this; 
-		while(ptr != NULL){
-			cout << ptr->data << endl;
-			ptr = ptr->next;
-		} // while
-	}
+    void printList() {
+        ListOf<TYPE> *ptr = this;
+        while (ptr != NULL) {
+            cout << ptr->data << endl;
+            ptr = ptr->next;
+        } // while
+    }
 };
 
-void main() {
-	ListOf<int>* anchor = new ListOf<int>(5);
-	anchor->append(4)->append(9)->append(10);
-	anchor->printList();
+int main() {
+    ListOf<int> *anchor = new ListOf<int>(5);
+    anchor->append(4)->append(9)->append(10);
+    anchor->printList();
 
-	cout << endl;
+    cout << endl;
 
-	ListOf<char>* anchor1 = new ListOf<char>('A');
-	anchor1->append('D')->append('E')->append('Z');
-	anchor1->printList();
+    ListOf<char> *anchor1 = new ListOf<char>('A');
+    anchor1->append('D')->append('E')->append('Z');
+    anchor1->printList();
 }
